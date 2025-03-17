37057e035ad002e14e3008a3071dcb5dcfeb6e36
okstring <milkoi7801@gmail.com>
2025-03-17 15:19:33 +0900

    simple: 지팡이 Constructor 수정

--- assignments/2025-03-17/wand.dart
+++ assignments/2025-03-17/wand.dart
@@ -21,15 +21,12 @@ class Wand {
     _power = value;
   }
 
-  Wand(this._name, this._power) {
+  Wand(String name, double power) : _name = name, _power = power {
     if (name.length < 3) {
       throw Exception('❎이름은 3자 이상이어야 합니다.');
     }
     if (power < 0.5 || power > 100.0) {
       throw Exception('❎지팡이의 마력은 0.5 이상 100.0 이하여야 합니다.');
     }
-
-    _name = name;
-    _power = power;
   }
 }
