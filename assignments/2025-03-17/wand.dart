3de003e6a427f5fc19456e9b1d3b04c3963c79e9
okstring <milkoi7801@gmail.com>
2025-03-17 14:28:08 +0900

    feat: wizard, wand 구현

---
+++ assignments/2025-03-17/wand.dart
@@ -0,0 +1,36 @@
+class Wand {
+  String _name;
+
+  String get name => _name;
+
+  set name(String value) {
+    if (value.length < 3) {
+      throw Exception('❎이름은 3자 이상이어야 합니다.');
+    }
+    _name = value;
+  }
+
+  double _power;
+
+  double get power => _power;
+
+  set power(double value) {
+    if (value < 0.5 || value > 100.0) {
+      throw Exception('❎지팡이의 마력은 0.5 이상 100.0 이하여야 합니다.');
+    }
+    _power = value;
+  }
+
+
+  Wand(this._name, this._power) {
+    if (name.length < 3) {
+      throw Exception('❎이름은 3자 이상이어야 합니다.');
+    }
+    if (power < 0.5 || power > 100.0) {
+      throw Exception('❎지팡이의 마력은 0.5 이상 100.0 이하여야 합니다.');
+    }
+
+    _name = name;
+    _power = power;
+  }
+}
---
+++ assignments/2025-03-17/wizard.dart
@@ -0,0 +1,58 @@
+import 'wand.dart';
+import 'dart:math';
+
+class Wizard {
+  String _name;
+  int _hp;
+  int _mp;
+  Wand? _wand;
+
+  String get name => _name;
+
+  set name(String value) {
+    if (value.length < 3) {
+      throw Exception('❎이름은 3자 이상이어야 합니다.');
+    }
+    _name = value;
+  }
+
+  int get hp => _hp;
+
+  set hp(int value) {
+    _hp = max(0, value);
+  }
+
+  int get mp => _mp;
+
+  set mp(int value) {
+    if (value < 0) {
+      throw Exception('❎마법사의 MP는 0 이상이어야 합니다.');
+    }
+    _mp = value;
+  }
+
+  Wand? get wand => _wand;
+
+  set wand(Wand? value) {
+    if (value == null) {
+      throw Exception('❎지팡이를 없앨 순 없습니다.');
+    }
+    _wand = value;
+  }
+
+  Wizard(String name, int hp, int mp, Wand? wand) :
+        _name = name,
+        _hp = max(0, hp),
+        _mp = mp,
+        _wand = wand {
+    if (name.length < 3) {
+      throw Exception('❎이름은 3자 이상이어야 합니다.');
+    }
+
+    if (mp < 0) {
+      throw Exception('❎마법사의 MP는 0 이상이어야 합니다.');
+    }
+  }
+}
+
+main() { }
