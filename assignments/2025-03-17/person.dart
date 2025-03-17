dffe80a93be7f7465d152a814e489e99ffff91a3
okstring <milkoi7801@gmail.com>
2025-03-17 14:28:17 +0900

    feat: person 구현

---
+++ assignments/2025-03-17/person.dart
@@ -0,0 +1,16 @@
+class Person {
+  final String name;
+  final int birthYear;
+
+  int get age {
+    final currentDateTime = DateTime.now();
+    final currentYear = currentDateTime.year;
+    return currentYear - birthYear;
+  }
+
+  Person({required this.name, required this.birthYear});
+}
+
+main() {
+
+}
