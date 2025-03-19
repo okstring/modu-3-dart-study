53d481453e6cf65cd73d7819e7a8418b23d614f6
okstring <milkoi7801@gmail.com>
2025-03-19 22:15:03 +0900

    simple: patent 간단 구현

--- lib/2025-03-19/model/patent.dart
+++ lib/2025-03-19/model/patent.dart
@@ -1,2 +1,10 @@
+import 'package:modu_3_dart_study/2025-03-19/index.dart';
 // 특허권
-class Patent {}
+class Patent extends IntangibleAsset {
+  Patent(super.name, super.createdDate, super.manageDepart, super.description);
+
+  @override
+  int calculateTax() {
+    throw UnimplementedError();
+  }
+}
