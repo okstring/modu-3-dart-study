eeaf2ec13a5787348db1f869705b2c5c1079f15b
okstring <milkoi7801@gmail.com>
2025-03-24 17:18:17 +0900

    remove: lib/ 반영 제거

--- lib/2025-03-24/string.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  final name = '오준석';
-  print(name);
-
-  print(name.substring(0, 2).toLowerCase());
-
-  final string = '1,2,3';
-  final List<String> splited = string.split(',');
-}
