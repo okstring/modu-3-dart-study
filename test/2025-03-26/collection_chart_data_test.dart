1a3a79011c7a9007cb88b3d1e57034e26c0b5a2a
okstring <milkoi7801@gmail.com>
2025-03-26 16:07:29 +0900

    test: collection_cart_data_list_model test 구현

---
+++ test/2025-03-26/collection_chart_data_test.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:collection/collection.dart';
+import 'package:test/test.dart';
+
+import '../../assignment/2025-03-26/index.dart';
+import '../../assignment/2025-03-25/default_file_operations.dart';
+
+void main() {
+  group('CollectionChartData 테스트', () {
+    final defaultFileOperations = DefaultFileOperations();
+    const sourcePath = 'assignment/2025-03-26/chart_data.json';
+
+    test('직렬화, 역직렬화가 정상적으로 이뤄지고 필드가 같아야 한다.', () {
+      // 역직렬화
+      final jsonString = defaultFileOperations.read(sourcePath);
+      final json = jsonDecode(jsonString);
+      final model = CollectionChartDataListModel.fromJson(json);
+
+      // 다시 직렬화
+      final serializationJson = model.toJson();
+      final serializationJsonString = jsonEncode(serializationJson);
+
+      // 테스트 위한 역직렬화
+      final deserializationJson = jsonDecode(serializationJsonString);
+      final deserializationModel = CollectionChartDataListModel.fromJson(
+        deserializationJson,
+      );
+
+      // 리스트 비교
+      final eq = ListEquality();
+      final isEqualList = eq.equals(
+        model.collectionChartDataList,
+        deserializationModel.collectionChartDataList,
+      );
+      expect(isEqualList, isTrue);
+    });
+  });
+}
