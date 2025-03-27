7c6eebd313ef1b5c86b0a86d136393280f27484c
okstring <milkoi7801@gmail.com>
2025-03-27 20:59:00 +0900

    test: movie_test.dart 구현

---
+++ test/2025-03-27/movie_test.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:test/test.dart';
+
+import '../../assignment/2025-03-25/default_file_operations.dart';
+import '../../assignment/2025-03-27/movie.dart';
+
+void main() {
+  group('Movie 테스트', () {
+  final defaultFileOperations = DefaultFileOperations();
+  const sourcePath = 'assignment/2025-03-27/movie.json';
+    test('직렬화, 역직렬화가 정상적으로 이뤄지고 필드가 같아야 한다.', () {
+      // 역직렬화
+      final jsonString = defaultFileOperations.read(sourcePath);
+      final json = jsonDecode(jsonString);
+      final movie = Movie.fromJson(json);
+
+      // 테스트 위한 다시 직렬화
+      final serializationJson = movie.toJson();
+      final serializationJsonString = jsonEncode(serializationJson);
+
+      // 테스트 위한 다시 역직렬화
+      final deserializationJson = jsonDecode(serializationJsonString);
+      final deserializationMovie = Movie.fromJson(deserializationJson);
+
+      // 비교
+      expect(movie, deserializationMovie);
+    });
+
+  test('getMovieInfo() 실행 시 딜레이가 2초 이상 걸려야 합니다.', () async {
+    final stopwatch = Stopwatch()..start();
+    final beforeTime = DateTime.now();
+
+    final _ = await getMovieInfo();
+
+    final timerTime = stopwatch.elapsed.inSeconds;
+    final afterTime = DateTime.now();
+    final diffTime = afterTime.difference(beforeTime).inSeconds;
+
+    expect(timerTime, greaterThanOrEqualTo(diffTime));
+  });
+  });
+}
