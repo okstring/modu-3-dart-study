b08895f9337960b4e597b66525ef024c8536ba75
okstring <milkoi7801@gmail.com>
2025-03-31 14:22:38 +0900

    test: user_data_source_test.dart 구현

---
+++ test/2025-03-31/user_data_source_test.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:test/test.dart';
+
+import '../../assignment/2025-03-31/user.dart';
+import '../../assignment/2025-03-31/user_data_source.dart';
+import '../../assignment/2025-03-31/user_data_source_impl.dart';
+
+void main() {
+  final UserDataSource userDataSource = UserDataSourceImpl();
+
+  group('User 테스트', () {
+    test('Users 직렬화, 역직렬화가 정상적으로 이뤄져야 한다.', () async {
+      final users = await userDataSource.getUsers();
+
+      final json = users.map((e) => e.toJson()).toList();
+      final jsonString = jsonEncode(json);
+      final List<dynamic> serializationJson = jsonDecode(jsonString);
+      final List<User> serializationTodos = serializationJson.map((e) => User.fromJson(e)).toList();
+
+      expect(users, equals(serializationTodos));
+    });
+  });
+}
