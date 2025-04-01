003ebba9958f9756e99c07e4b71117235f2955a9
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-04-01 14:09:55 +0900

    feat: Repository 과제 샘플

--- lib/2025-03-31/data_source/stock_data_source_impl.dart
+++ lib/2025-03-31/data_source/stock_data_source_impl.dart
@@ -4,13 +4,12 @@ import 'package:modu_3_dart_study/2025-03-31/data_source/stock_data_source.dart'
 import 'package:modu_3_dart_study/2025-03-31/model/stock_listing.dart';
 
 class StockDataSourceImpl implements StockDataSource {
-  final File _stockFile = File(
-    '/Users/junsuk/dev/FlutterProjects/modu-3-dart-study/lib/2025-03-31/listing_status.csv',
-  );
+  final FileOperator myFile = MockOperatorImpl();
 
   @override
   Future<List<StockListing>> getStockListings() async {
-    final List<String> csvList = await _stockFile.readAsLines();
+    final String text = await myFile.readAsLines();
+    final List<String> csvList = text.split('\n');
     csvList.removeAt(0);
 
     return csvList
@@ -18,4 +17,29 @@ class StockDataSourceImpl implements StockDataSource {
         .where((e) => e.name?.isNotEmpty ?? false)
         .toList();
   }
+}
+
+abstract interface class FileOperator {
+  Future<String> readAsLines();
+}
+
+class MockOperatorImpl implements FileOperator {
+  @override
+  Future<String> readAsLines() async {
+    return """symbol,name,exchange,assetType,ipoDate,delistingDate,status
+A,Agilent Technologies Inc,NYSE,Stock,1999-11-18,null,Active
+AA,Alcoa Corp,NYSE,Stock,2016-10-18,null,Active""";
+  }
+}
+
+class FileOperatorImpl implements FileOperator {
+  final File _stockFile = File(
+    '/Users/junsuk/dev/FlutterProjects/modu-3-dart-study/lib/2025-03-31/listing_status.csv',
+  );
+
+  @override
+  Future<String> readAsLines() async {
+    return await _stockFile.readAsString();
+  }
+
 }
