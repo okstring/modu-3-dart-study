7c8843f75cfb28b9a0d85745277725ba9f345630
okstring <milkoi7801@gmail.com>
2025-04-03 23:20:47 +0900

    feat: MovieDataSource 테스트, MockHttpClient 목업 코드 작성

---
+++ test/2025-04-03/data_source/remote/movie_data_source_impl_test.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:modu_3_dart_study/2025-04-03/data_source/movie_data_source.dart';
+import 'package:modu_3_dart_study/2025-04-03/data_source/remote/movie_data_source_impl.dart';
+import 'package:modu_3_dart_study/2025-04-03/model/movieList.dart';
+import 'package:modu_3_dart_study/2025-04-03/repository/movie_repository.dart';
+import 'package:modu_3_dart_study/2025-04-03/repository/movie_repository_impl.dart';
+import 'package:test/test.dart';
+
+import '../../network/mock_http_client_impl.dart';
+
+void main() {
+  group('MovieDataSource 테스트', () {
+    const body = {
+      "results": [
+        {
+          "id": 939243,
+          "title": "수퍼 소닉 3",
+          "overview": "너클즈, 테일즈와 함께 평화로운 일상을 보내던 초특급 히어로 소닉...",
+          "release_date": "2024-12-19",
+        },
+      ],
+    };
+    const urlString = MovieDataSourceImpl.urlString;
+
+    final mockHttpClient = MockHttpClientImpl(urlString: urlString, body: body);
+    final MovieDataSource movieDataSource = MovieDataSourceImpl(client: mockHttpClient.mockClient);
+    final MovieRepository repository = MovieRepositoryImpl(movieDataSource: movieDataSource);
+
+    test('MockHttpClient 테스트', () async {
+      final movieList = await repository.getMovieInfoList();
+
+      final json = body;
+      final anotherMovieList = MovieList.fromJson(json);
+
+      expect(movieList.results.length, equals(anotherMovieList.results.length));
+
+      if (movieList.results.isNotEmpty) {
+        expect(movieList.results.first.id, anotherMovieList.results.first.id);
+      }
+    });
+  });
+}
--- test/2025-04-03/network/mock_http_client_impl.dart
+++ test/2025-04-03/network/mock_http_client_impl.dart
@@ -2,25 +2,20 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:http/testing.dart';
-import 'package:modu_3_dart_study/2025-04-03/data_source/remote/movie_data_source_impl.dart';
 
 class MockHttpClientImpl {
-  static const Map<String, List<Map<String, dynamic>>> body = {
-    "results": [
-      {
-        "id": 939243,
-        "title": "수퍼 소닉 3",
-        "overview": "너클즈, 테일즈와 함께 평화로운 일상을 보내던 초특급 히어로 소닉...",
-        "release_date": "2024-12-19",
-      },
-    ],
-  };
+  final String urlString;
+  final Map<String, List<Map<String, dynamic>>> body;
 
-  final mockClient = MockClient((request) async {
-    if (request.url.toString() == MovieDataSourceImpl.urlString) {
-      return http.Response.bytes(utf8.encode(jsonEncode(body)), 200);
-    } else {
-      return http.Response('Not Found', 404);
-    }
-  });
+  MockHttpClientImpl({required this.urlString, required this.body});
+
+  MockClient get mockClient {
+    return MockClient((request) async {
+      if (request.url.toString() == urlString) {
+        return http.Response.bytes(utf8.encode(jsonEncode(body)), 200);
+      } else {
+        return http.Response('Not Found', 404);
+      }
+    });
+  }
 }
